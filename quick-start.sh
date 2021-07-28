ac7f3315102b143c731acf575a687462715b9fc6
Antonio Aloisio <antonio.aloisio@zalando.de>
2021-07-28 07:45:59 +0200

    added update-docker-image.sh

--- quick-start.sh
+++ quick-start.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-mkdir ft_userdata
-cd ft_userdata/
-# Download the docker-compose file from the repository
-curl https://raw.githubusercontent.com/freqtrade/freqtrade/stable/docker-compose.yml -o docker-compose.yml
-
-# Pull the freqtrade image
-docker-compose pull
+# download for the first time docker image
+./update-docker-image.sh
 
 # Create user directory structure
 docker-compose run --rm freqtrade create-userdir --userdir user_data
